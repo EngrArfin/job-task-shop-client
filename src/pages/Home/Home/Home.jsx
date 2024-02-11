4c78cbd73506c2be8cac8a079868d7857df89b4a
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-02-11 12:01:30 +0600

    navbar footer and header

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Share/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home pages</h2>
-            <Link to='home' className="btn">Home</Link>
-            <Link to='userhome' className="btn">user</Link>
-            <Link to='adminhome' className="btn">admin</Link>
-            <Link to='home' className="btn">Login</Link>
-            
+           
+           <Header></Header>
+            <Footer></Footer>
         </div>
     );
 };
