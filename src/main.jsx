ec7e1eb42fd8a55472b27ea33bc1df47e70bd563
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-02-24 17:51:54 +0600

    AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import {
 } from "react-router-dom";
 
 import { router } from './Router/Router.jsx';
+import AuthProvider from './AuthProvider/AuthProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className=' max-w-8xl max-auto '> {/* max-w-8xl mx-auto */}
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className=' max-w-8xl max-auto '> {/* max-w-8xl mx-auto */}
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
